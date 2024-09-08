c94ef4c1049f8c8e4c97ff011e7687f2b7c9dbf2
Alexander <str_999@bk.ru>
2024-09-08 08:24:53 +0500

    Initial

---
+++ Fib1/Fib1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int fib(int n);
+
+int main() {
+	std::cout << fib(18);
+
+	return 0;
+}
+
+int fib(int n) {
+	if (n == 0 || n == 1) {
+		return n;
+	}
+
+	return fib(n - 1) + fib(n - 2);
+}
+
---
+++ Fib2/Fib2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+long long fib(int n);
+
+int main() {
+
+	try {
+		std::cout << fib(665);
+
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+
+	
+
+}
+
+long long fib(int n) {
+	if(n >= 2) {
+	long long* arr = new long long[n];
+	arr[0] = 1;
+	arr[1] = 1;
+	for (int i = 2; i < n; ++i) {
+		arr[i] = arr[i - 1] + arr[i - 2];
+
+
+	}
+
+	long long  a = arr[n - 1];
+	delete[] arr;
+	return a;
+	}
+	else if (n == 1) {
+		return 1;
+	}
+	else { throw std::invalid_argument("Quantity of elements must be greater than zero."); }
+	
+
+	
+}
